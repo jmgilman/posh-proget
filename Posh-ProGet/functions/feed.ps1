8cc6c01b52b4dfd075c2f2c4deb3663a4d8bc36c
Joshua Gilman <joshuagilman@gmail.com>
2021-03-28 20:28:17 -0700

    Adds functions for generating other feed related objects

--- Posh-ProGet/functions/feed.ps1
+++ Posh-ProGet/functions/feed.ps1
@@ -34,6 +34,110 @@ Function New-FeedObject {
 
 <#
 .SYNOPSIS
+    Returns a new Connector object with the optional properties set
+.DESCRIPTION
+    Returns a new instance of the Connector object. Additional properties in the form
+    of a dictionary can be passed to initialize the object with the given
+    property values.
+.PARAMETER Properties
+    A dictionary of property values to initialize the Connector with
+.EXAMPLE
+    $conn = New-ConnectorObject @{Name='chocolatey-conn'; FeedType='chocolatey'}
+.OUTPUTS
+    A new instance of the Connector object
+#>
+Function New-ConnectorObject {
+    param(
+        [Parameter(
+            Mandatory = $false,
+            Position = 1
+        )]
+        $Properties
+    )
+
+    New-Object Connector -Property $Properties
+}
+
+<#
+.SYNOPSIS
+    Returns a new RetentionRule object with the optional properties set
+.DESCRIPTION
+    Returns a new instance of the RetentionRule object. Additional properties in the form
+    of a dictionary can be passed to initialize the object with the given
+    property values.
+.PARAMETER Properties
+    A dictionary of property values to initialize the RetentionRule with
+.EXAMPLE
+    $rule = New-RetentionRuleObject @{DeleteCached=$true; KeepVersionsCount=10}
+.OUTPUTS
+    A new instance of the RetentionRule object
+#>
+Function New-RetentionRuleObject {
+    param(
+        [Parameter(
+            Mandatory = $false,
+            Position = 1
+        )]
+        $Properties
+    )
+
+    New-Object RetentionRule -Property $Properties
+}
+
+<#
+.SYNOPSIS
+    Returns a new ReplicationData object with the optional properties set
+.DESCRIPTION
+    Returns a new instance of the ReplicationData object. Additional properties in the form
+    of a dictionary can be passed to initialize the object with the given
+    property values.
+.PARAMETER Properties
+    A dictionary of property values to initialize the ReplicationData with
+.EXAMPLE
+    $rep = New-ReplicationDataObject @{ClientMode='mirror'; ServerMode='readonly'}
+.OUTPUTS
+    A new instance of the ReplicationData object
+#>
+Function New-ReplicationDataObject {
+    param(
+        [Parameter(
+            Mandatory = $false,
+            Position = 1
+        )]
+        $Properties
+    )
+
+    New-Object ReplicationData -Property $Properties
+}
+
+<#
+.SYNOPSIS
+    Returns a new LicenseData object with the optional properties set
+.DESCRIPTION
+    Returns a new instance of the LicenseData object. Additional properties in the form
+    of a dictionary can be passed to initialize the object with the given
+    property values.
+.PARAMETER Properties
+    A dictionary of property values to initialize the LicenseData with
+.EXAMPLE
+    $lic = New-LicenseDataObject @{LicenseId='myid'}
+.OUTPUTS
+    A new instance of the LicenseData object
+#>
+Function New-LicenseDataObject {
+    param(
+        [Parameter(
+            Mandatory = $false,
+            Position = 1
+        )]
+        $Properties
+    )
+
+    New-Object LicenseData -Property $Properties
+}
+
+<#
+.SYNOPSIS
     Fetches a list of all feeds from the ProGet API
 .DESCRIPTION
     Uses the given ProGet session to connect to the feeds API endpoint and fetch
